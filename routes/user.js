814a2593710b587d1ce819a6e0a7da8b7250e10e
Rahul Pranami <69789945+RahulPranami@users.noreply.github.com>
2022-12-16 14:11:06 +0530

    small Modification

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 router.post("/signup", (req, res) => {
   let user = req.body;
-  query = "select email, password ,role ,status from user where email=?";
+  query = "select email,password,role,status from user where email=?";
   connection.query(query, [user.email], (err, results) => {
     if (!err) {
       if (results.length <= 0) {
         query =
-          "insert into user(name,contactNumber , email ,password ,status, role) values(?,?,?,?,'false','user')";
+          "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
         connection.query(
           query,
           [user.name, user.contactNumber, user.email, user.password],
